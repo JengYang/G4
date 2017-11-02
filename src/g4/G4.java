19e0b5aba7bc44c5f332f045708ff949f71d316d
Student <Student@PC84>
2017-11-02 15:41:49 +0800

    ...

--- src/g4/G4.java
+++ src/g4/G4.java
@@ -16,7 +16,7 @@ public class G4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("main");
+        System.out.println("aaaaaaaaaaaa");
     }
     
 }
